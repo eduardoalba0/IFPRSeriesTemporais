bcddbfcd084066cd7e72e3fb0680800983572d40
Eduardo Luiz Albam <eduardoalba0@hotmail.com>
2023-10-09 23:48:08 -0300

    Initial Commit

--- main.py
+++ main.py
@@ -5,28 +5,12 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 from sklearn.model_selection import train_test_split
-from tabulate import tabulate
 
 
-def prepararDados():
-    dfAgua = pd.read_csv("./Dados/ConsumoAguaIFPR.csv", delimiter=";")
-    dfEnergia = pd.read_csv("./Dados/ConsumoEnergiaIFPR.csv", delimiter=";")
-    dfClima = pd.read_csv("./Dados/ClimaINMETClevelandia.csv", delimiter=";")
-    dfAgua["DATA LEITURA"] = pd.to_datetime(dfAgua["DATA LEITURA"], format="%d/%m/%Y")
-    dfEnergia["DATA LEITURA"] = pd.to_datetime(dfEnergia["DATA LEITURA"], format="%d/%m/%Y")
-    dfClima["DATA"] = pd.to_datetime((dfClima["DATA"] + " " + dfClima["HORA"]),
-                                     format="%d/%m/%Y %H:%M")
-
-    dfAgua.set_index("DATA LEITURA", inplace=True)
-    dfEnergia.set_index("DATA LEITURA", inplace=True)
-    dfClima.set_index("DATA", inplace=True)
-
-    return dfAgua, dfEnergia, dfClima
-
-
-def plotBasico(x, y, titulo="", xlabel="Data", ylabel=""):
+def plotBasico(df, titulo="", xlabel="Data", ylabel=""):
     plt.figure(figsize=(20, 10), dpi=100)
-    plt.plot(x, y, color="tab:blue")
+    for values in df.columns.values:
+        plt.plot(df.index.values, df[values], label=values)
     plt.gca().set(title=titulo, xlabel=xlabel, ylabel=ylabel)
     plt.xticks(rotation=90)
     plt.legend()
@@ -43,49 +27,114 @@ def plotTreinoTeste(x, valoresPrevistos, valoresEsperados, title="", xlabel="Dat
     plt.show()
 
 
-def validar(df, var):
+def prepararDados():
+    dfAgua = pd.read_csv("./Dados/ConsumoAguaIFPR.csv", delimiter=";")
+    dfEnergia = pd.read_csv("./Dados/ConsumoEnergiaIFPR.csv", delimiter=";")
+    dfClima = pd.read_csv("./Dados/ClimaINMETClevelandia.csv", delimiter=";")
+    dfAgua["DATA LEITURA"] = pd.to_datetime(dfAgua["DATA LEITURA"], format="%d/%m/%Y")
+    dfEnergia["DATA LEITURA"] = pd.to_datetime(dfEnergia["DATA LEITURA"], format="%d/%m/%Y")
+    dfClima["DATA"] = pd.to_datetime((dfClima["DATA"] + " " + dfClima["HORA"]),
+                                     format="%d/%m/%Y %H:%M")
+
+    dfAgua.set_index("DATA LEITURA", inplace=True)
+    dfEnergia.set_index("DATA LEITURA", inplace=True)
+    dfClima.set_index("DATA", inplace=True)
+
+    for coluna in dfAgua.columns[1:]:
+        dfAgua[coluna] = dfAgua[coluna].astype(float)
+
+    for coluna in dfEnergia.columns[1:]:
+        dfEnergia[coluna] = dfEnergia[coluna].astype(float)
+
+    return dfAgua, dfEnergia, dfClima
+
+
+def explorarDados(dfAgua, dfEnergia, dfClima):
+    print(dfAgua.columns[dfAgua.isnull().any()])
+    print(dfEnergia.columns[dfEnergia.isnull().any()])
+    print(dfClima.columns[dfClima.isnull().any()])
+
+    plotBasico(dfAgua,
+               titulo="Consumo de Água no IFPR - Campus Palmas no período de Agosto de 2018 a Setembro de 2023")
+    plotBasico(dfEnergia,
+               titulo="Consumo de Energia Elétrica no IFPR - Campus Palmas no período de Agosto de 2018 a Setembro de 2023")
+    plotBasico(dfClima.dropna(),
+               titulo="Condições Climáticas da região de Palmas no período de Agosto de 2018 a Setembro de 2023")
+
+
+def treinarRF(df, var):
     y = df[var]
     x = df.drop(var, axis=1)
+
     xTreino, xTeste, yTreino, yTeste = train_test_split(x, y, test_size=0.3, random_state=2023)
 
     modelo = RandomForestRegressor(n_estimators=1000, max_depth=10, random_state=2023)
     modelo.fit(xTreino, yTreino)
-    yPrevisao = modelo.predict(xTeste)
-    tamTreino = len(xTreino)
-    tamTeste = len(xTeste)
 
-    mae = mean_absolute_error(yTeste, yPrevisao)
-    mape = mean_absolute_percentage_error(yTeste, yPrevisao)
-    mse = mean_squared_error(yTeste, yPrevisao)
-    rmse = np.sqrt(mse)
+    print('Conjunto de Treino: %d' % len(yTreino))
+    print('Conjunto de Teste: %d' % len(yTeste))
 
-    return mae, mape, mse, rmse, pd.DataFrame(yPrevisao).rename(columns={0: var}), pd.DataFrame(yTeste), tamTreino, tamTeste
+    return modelo, xTreino, xTeste, yTreino, yTeste
+
+
+def testar(modelo, variaveis, gabarito=[]):
+    yPrevisto = modelo.predict(variaveis)
+
+    if isinstance(gabarito, pd.DataFrame):
+        mae = mean_absolute_error(gabarito, yPrevisto)
+        mape = mean_absolute_percentage_error(gabarito, yPrevisto)
+        mse = mean_squared_error(gabarito, yPrevisto)
+        rmse = np.sqrt(mse)
+
+        print('MAE: %.3f' % mae)
+        print('MAPE: %.3f' % mape)
+        print('MSE: %.3f' % mse)
+        print('RMSE: %.3f' % rmse)
+
+    return yPrevisto
+
+
+def previsaoAgua(dfAgua):
+    modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(dfAgua, ["CONSUMO (M3)"])
+    yPrevisto = testar(modelo, xTeste, yTeste)
+
+    dfAux = pd.DataFrame(yPrevisto)
+    dfAux["DATA LEITURA"] = yTeste.index.values
+
+    dfPrevisao = dfAgua.copy()
+    for val, data in dfAux.values:
+        dfPrevisao.loc[data, "CONSUMO (M3)"] = val
+
+    plotTreinoTeste(dfAgua.index.values,
+                    dfAgua["CONSUMO (M3)"],
+                    dfPrevisao["CONSUMO (M3)"])
+
+
+def previsaoEnergia(dfEnergia):
+    modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(dfEnergia, ["ENERGIA ELÉTRICA PONTA (KWh)",
+                                                                     "ENERGIA ELÉTRICA FORA DA PONTA (KWh)"])
+    yPrevisto = testar(modelo, xTeste, yTeste)
+
+    dfAux = pd.DataFrame(yPrevisto)
+    dfAux["DATA LEITURA"] = yTeste.index.values
+
+    dfPrevisao = dfEnergia.copy()
+    dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"] = dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"].astype(float)
+    dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"] = dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"].astype(
+        float)
+    for val1, val2, data in dfAux.values:
+        dfPrevisao.loc[data, "ENERGIA ELÉTRICA PONTA (KWh)"] = val1
+        dfPrevisao.loc[data, "ENERGIA ELÉTRICA FORA DA PONTA (KWh)"] = val2
+
+    plotTreinoTeste(dfEnergia.index.values,
+                    dfEnergia["ENERGIA ELÉTRICA PONTA (KWh)"],
+                    dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"])
+
+    plotTreinoTeste(dfEnergia.index.values,
+                    dfEnergia["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"],
+                    dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"])
+
 
 
 dfAgua, dfEnergia, dfClima = prepararDados()
-
-mae, mape, mse, rmse, valoresPrevistos, valoresEsperados, tamTreino, tamTeste = validar(dfEnergia, "CONSUMO (KWh)")
-print('Conjunto de Treino: %d' % tamTreino)
-print('Conjunto de Teste: %d' % tamTeste)
-print('MAE: %.3f' % mae)
-print('MAPE: %.3f' % mape)
-print('MSE: %.3f' % mse)
-print('RMSE: %.3f' % rmse)
-
-valoresPrevistos["DATA LEITURA"] = valoresEsperados.index.values
-dfPrevisao = dfEnergia.copy()
-for val, data in valoresPrevistos.values:
-    dfPrevisao.loc[data, "CONSUMO (KWh)"] = val
-
-plotTreinoTeste(dfEnergia.index.values,
-                dfEnergia["CONSUMO (KWh)"],
-                dfPrevisao["CONSUMO (KWh)"])
-
-'''
-plotBasico(x=dfAgua["DATA LEITURA"], y=dfAgua['CONSUMO (M3)'],
-           title="Consumo de Água no IFPR - Campus Palmas no período de Agosto de 2018 a Setembro de 2023")
-plotBasico(x=dfEnergia["DATA LEITURA"], y=dfEnergia['CONSUMO (KWh)'],
-           title="Consumo de Energia Elétrica no IFPR - Campus Palmas no período de Agosto de 2018 a Setembro de 2023")
-plotBasico(x=dfClima["DATA (YYYY-MM-DD)"], y=dfClima["TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"],
-           title="Condições Climáticas da região de Palmas no período de Agosto de 2018 a Setembro de 2023")
-'''
+previsaoEnergia(dfEnergia)
