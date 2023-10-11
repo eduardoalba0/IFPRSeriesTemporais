1b28830c232983bf96d6f106afaae140c74c8e33
Eduardo Luiz Albam <eduardoalba0@hotmail.com>
2023-10-11 00:47:31 -0300

    Fix Pre Processamento do Clima

--- main.py
+++ main.py
@@ -38,13 +38,17 @@ def prepararDados():
 
     dfAgua.set_index("DATA LEITURA", inplace=True)
     dfEnergia.set_index("DATA LEITURA", inplace=True)
-    dfClima.set_index("DATA", inplace=True)
 
-    for coluna in dfAgua.columns[1:]:
+    for coluna in dfAgua.columns:
         dfAgua[coluna] = dfAgua[coluna].astype(float)
 
-    for coluna in dfEnergia.columns[1:]:
+    for coluna in dfEnergia.columns:
         dfEnergia[coluna] = dfEnergia[coluna].astype(float)
+
+    dfClima.dropna(inplace=True, how="all")
+    dfClima.drop("HORA", axis=1, inplace=True)
+    for coluna in dfClima.columns[1:]:
+        dfClima[coluna] = dfClima[coluna].astype(float)
 
     return dfAgua, dfEnergia, dfClima
 
@@ -66,9 +70,10 @@ def treinarRF(df, var):
     y = df[var]
     x = df.drop(var, axis=1)
 
-    xTreino, xTeste, yTreino, yTeste = train_test_split(x, y, test_size=0.3, random_state=2023)
+    xTreino, xTeste, yTreino, yTeste = train_test_split(x, y.values.ravel(), test_size=0.25, random_state=2023)
 
     modelo = RandomForestRegressor(n_estimators=1000, max_depth=10, random_state=2023)
+
     modelo.fit(xTreino, yTreino)
 
     print('Conjunto de Treino: %d' % len(yTreino))
@@ -77,10 +82,10 @@ def treinarRF(df, var):
     return modelo, xTreino, xTeste, yTreino, yTeste
 
 
-def testar(modelo, variaveis, gabarito=[]):
+def testar(modelo, variaveis, gabarito=None):
     yPrevisto = modelo.predict(variaveis)
 
-    if isinstance(gabarito, pd.DataFrame):
+    if gabarito is not None and np.any(gabarito):
         mae = mean_absolute_error(gabarito, yPrevisto)
         mape = mean_absolute_percentage_error(gabarito, yPrevisto)
         mse = mean_squared_error(gabarito, yPrevisto)
@@ -94,47 +99,53 @@ def testar(modelo, variaveis, gabarito=[]):
     return yPrevisto
 
 
+def preProcessar(df):
+    df = df.groupby(pd.Grouper(key="DATA", freq="D")).mean()
+    for coluna in df.columns[df.isnull().any()]:
+        modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(df.dropna(), [coluna])
+        yPrevisto = testar(modelo, xTeste, yTeste)
+        dfAux = pd.DataFrame(xTeste)
+        dfAux["PREVISÃO"] = yPrevisto
+        dfAux["ESPERADO"] = yTeste
+
+        dfAux.sort_index(inplace=True)
+        plotTreinoTeste(dfAux.index.values, dfAux["PREVISÃO"], dfAux["ESPERADO"], title=coluna)
+
+
 def previsaoAgua(dfAgua):
     modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(dfAgua, ["CONSUMO (M3)"])
     yPrevisto = testar(modelo, xTeste, yTeste)
+    dfAux = pd.DataFrame(xTeste)
+    dfAux["PREVISÃO"] = yPrevisto
+    dfAux["ESPERADO"] = yTeste
 
-    dfAux = pd.DataFrame(yPrevisto)
-    dfAux["DATA LEITURA"] = yTeste.index.values
-
-    dfPrevisao = dfAgua.copy()
-    for val, data in dfAux.values:
-        dfPrevisao.loc[data, "CONSUMO (M3)"] = val
-
-    plotTreinoTeste(dfAgua.index.values,
-                    dfAgua["CONSUMO (M3)"],
-                    dfPrevisao["CONSUMO (M3)"])
-
+    dfAux.sort_index(inplace=True)
+    plotTreinoTeste(dfAux.index.values, dfAux["PREVISÃO"], dfAux["ESPERADO"], title="CONSUMO (M3)")
 
 def previsaoEnergia(dfEnergia):
     modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(dfEnergia, ["ENERGIA ELÉTRICA PONTA (KWh)",
                                                                      "ENERGIA ELÉTRICA FORA DA PONTA (KWh)"])
     yPrevisto = testar(modelo, xTeste, yTeste)
+    dfAux = pd.DataFrame(xTeste)
 
-    dfAux = pd.DataFrame(yPrevisto)
-    dfAux["DATA LEITURA"] = yTeste.index.values
-
+    print(yPrevisto)
+    print(yTeste)
     dfPrevisao = dfEnergia.copy()
     dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"] = dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"].astype(float)
     dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"] = dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"].astype(
         float)
-    for val1, val2, data in dfAux.values:
-        dfPrevisao.loc[data, "ENERGIA ELÉTRICA PONTA (KWh)"] = val1
-        dfPrevisao.loc[data, "ENERGIA ELÉTRICA FORA DA PONTA (KWh)"] = val2
 
     plotTreinoTeste(dfEnergia.index.values,
-                    dfEnergia["ENERGIA ELÉTRICA PONTA (KWh)"],
-                    dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"])
+                    dfPrevisao["ENERGIA ELÉTRICA PONTA (KWh)"],
+                    dfEnergia["ENERGIA ELÉTRICA PONTA (KWh)"])
 
     plotTreinoTeste(dfEnergia.index.values,
-                    dfEnergia["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"],
-                    dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"])
-
+                    dfPrevisao["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"],
+                    dfEnergia["ENERGIA ELÉTRICA FORA DA PONTA (KWh)"])
 
 
 dfAgua, dfEnergia, dfClima = prepararDados()
-previsaoEnergia(dfEnergia)
+previsaoAgua(dfAgua)
+#preProcessar(dfClima)
+# dfClima = dfClima.groupby(pd.Grouper(key="DATA", freq="M")).mean()
+# previsaoEnergia(dfEnergia)
