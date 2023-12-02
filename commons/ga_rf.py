3091468748bfe728757ca8f4123481d082cb214b
Eduardo Luiz Albam <eduardoalba0@hotmail.com>
2023-12-02 10:35:14 -0300

    Melhora no Ga.

--- commons/ga_rf.py
+++ commons/ga_rf.py
@@ -1,93 +1,98 @@
-import numpy as np
 import random
-import math
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
+import threading
 
 from commons.treinoTeste import treinarRF, testar
 
 
 class GARF:
-    def __init__(self, dados, variaveis, n_individuos, n_geracoes, semente):
+    def __init__(self, dados, variaveis, n_individuos, n_geracoes, tx_mutacao, semente=None):
+        random.seed(semente)
         self.dados = dados
         self.variaveis = variaveis
         self.n_individuos = n_individuos
         self.n_geracoes = n_geracoes
-        self.semente = semente
-        self.init_populacao()
+        self.tx_mutacao = tx_mutacao
 
     def run(self):
-        while not self.stop_condition:
-            self.fitness_populacao()
-            self.best()
-            self.crossover()
-            self.mutation()
+        self.init_populacao()
+        for _ in range(self.n_geracoes):
+            self.calcular_fitness()
+            filho = self.crossover()
+            filho = self.mutation(filho)
+            self.populacao.append(filho)
+            self.populacao.pop(len(self.populacao) - 1)
+        return self.populacao[0]
 
     def init_populacao(self):
         self.populacao = []
         for _ in range(self.n_individuos):
-            # Cada lag gerado irá comprometer 1 dado de treinamento, não é bom exagerar
-            n_lags = random.randint(0, self.dados.shape[0] / 2)
-            # n_estimators é o número de árvores com combinações diferentes de dados e comparações (50 ou menos já geram previsões precisas. mais de 200 aumenta o tempo de execução)
-            n_estimators = random.randint(2, 200)
-            # max_depth não pode ultrapassar o número de variáveis
-            max_depth = random.randint(1, self.dados.shape[1] + n_lags)
-            # min_sample_leaf não pode ser maior do que os valores de entrada (observações)
-            min_sample_leaf = random.randint(1, self.dados.shape[0])
-            individuo = IndividuoRF(n_estimators, max_depth, min_sample_leaf, n_lags)
+            individuo = IndividuoRF()
             self.populacao.append(individuo)
 
-    def fitness_populacao(self):
-        for individuo in self.populacao:
-            if individuo.fitness == None:
-                modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(self.dados, self.variaveis, individuo.n_estimators,
-                                                                     individuo.max_depth, individuo.min_sample_leaf,
-                                                                     individuo.n_lags)
-                dfResultado, dfResumo = testar(modelo, xTeste, yTeste)
-                individuo.fitness = dfResumo.loc[0, "MSE"]
+    def calcular_fitness(self):
+        print(self.populacao)
+        threads = []
+        for individuo in filter(lambda individuo: individuo.fitness == None, self.populacao):
+            thread = threading.Thread(target=self.thread_calc_fitness(individuo))
+            threads.append(thread)
+            thread.start()
 
-    def best(self):
-        # Encontra o melhor indivíduo da população
-        melhor_individuo = self.populacao[0]
-        for individuo in self.populacao:
-            if individuo.fitness > melhor_individuo.fitness:
-                melhor_individuo = individuo
+        for thread in threads:
+            thread.join()
 
-        return melhor_individuo
+        self.populacao = sorted(self.populacao, key=lambda individuo: individuo.fitness)
 
-    def tournament(self):
-        # Seleciona dois indivíduos da população aleatoriamente
-        individuo_1 = random.choice(self.populacao)
-        individuo_2 = random.choice(self.populacao)
+    def thread_calc_fitness(self, individuo):
+        modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(self.dados, self.variaveis, individuo.n_estimators,
+                                                             individuo.max_depth, individuo.min_sample_leaf,
+                                                             individuo.n_lags)
+        dfResultado, dfResumo = testar(modelo, xTeste, yTeste)
+        individuo.fitness = dfResumo.loc[0, "MSE (KWh)"]
 
-        # Compara os dois indivíduos e retorna o melhor
-        if individuo_1.fitness > individuo_2.fitness:
-            return individuo_1
-        else:
-            return individuo_2
+    def crossover(self):
+        pai = random.choice(self.populacao)
+        mae = random.choice(self.populacao)
+        filho = IndividuoRF()
+        filho.n_lags = random.choice([pai.n_lags, mae.n_lags])
+        filho.n_estimators = random.choice([pai.n_estimators, mae.n_estimators])
+        filho.max_depth = random.choice([pai.max_depth, mae.max_depth])
+        filho.min_sample_leaf = random.choice([pai.min_sample_leaf, mae.min_sample_leaf])
 
-    def crossover(self, individuo_1, individuo_2):
-        # Seleciona um ponto de corte aleatório
-        ponto_de_corte = random.randint(0, len(individuo_1.PARAMETROS) - 1)
-
-        # Cria dois novos indivíduos
-        individuo_filho_1 = individuo_1[:ponto_de_corte] + individuo_2[ponto_de_corte:]
-        individuo_filho_2 = individuo_2[:ponto_de_corte] + individuo_1[ponto_de_corte:]
-
-        return individuo_filho_1, individuo_filho_2
+        return filho
 
     def mutation(self, individuo):
-        # Seleciona um parâmetro aleatório
-        parametro = random.choice(individuo.parametros)
-
-        # Altera o valor do parâmetro
-        individuo.parametros[parametro] = random.uniform(0, 1)
+        if random.uniform(0, 1) < self.tx_mutacao:
+            individuo.n_lags = round(individuo.n_lags * (random.uniform(0.8, 1.2)))
+        if random.uniform(0, 1) < self.tx_mutacao:
+            individuo.n_estimators = round(individuo.n_estimators * (random.uniform(0.8, 1.2)))
+        if random.uniform(0, 1) < self.tx_mutacao:
+            individuo.max_depth = round(individuo.max_depth * (random.uniform(0.8, 1.2)))
+        if random.uniform(0, 1) < self.tx_mutacao:
+            individuo.min_sample_leaf = round(individuo.min_sample_leaf * (random.uniform(0.8, 1.2)))
+        return individuo
 
 
 class IndividuoRF:
+    def __init__(self):
+        self.rand_n_lags()
+        self.rand_n_estimators()
+        self.rand_max_depth()
+        self.rand_min_sample_leaf()
+        self.fitness = None
 
-    def __init__(self, n_estimators, max_depth, min_sample_leaf, n_lags):
-        self.n_estimators = n_estimators
-        self.max_depth = max_depth
-        self.min_sample_leaf = min_sample_leaf
-        self.n_lags = n_lags
+    def rand_n_lags(self):
+        self.n_lags = random.randint(0, 20)
+
+    def rand_n_estimators(self):
+        self.n_estimators = random.randint(2, 200)
+
+    def rand_min_sample_leaf(self):
+        self.min_sample_leaf = random.randint(1, 50)
+
+    def rand_max_depth(self):
+        self.max_depth = random.randint(0, 200)
+        if self.max_depth == 0:
+            self.max_depth = None
+
+    def __str__(self):
+        return f'n_lags: {self.n_lags}, n_estimators: {self.n_estimators}, max_depth: {self.max_depth}, min_sample_leaf: {self.min_sample_leaf}'
--- eletricidade.py
+++ eletricidade.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from commons.exploracao import plotTreinoTeste, acf
+from commons.ga_rf import GARF
 from commons.preprocessamento import prepararDados, agrupamentoMensalMedia
 from commons.treinoTeste import treinarRF, testar
 
@@ -17,7 +18,13 @@ dfEnergia = dfEnergia.iloc[1:, :]
 dfMerged = dfEnergia.merge(dfHorasAula, right_index=True, left_index=True, how="inner")
 dfMerged = dfMerged.merge(dfClima, right_index=True, left_index=True, how="inner")
 
-modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(dfMerged, variavel, nLags=3)
+ga_rf = GARF(dfMerged, variavel, 50, 2000, 0.05, 1234)
+best = ga_rf.run()
+print(best)
+
+modelo, xTreino, xTeste, yTreino, yTeste = treinarRF(dfMerged, variavel, estimators=best.n_estimators,
+                                                     maxDepth=best.max_depth, minSampleLeaf=best.min_sample_leaf,
+                                                     nLags=best.n_lags)
 
 print("Conjunto de Treinamento: %d" % len(xTreino))
 print("Conjunto de Teste: %d" % len(xTeste))
@@ -25,5 +32,5 @@ print("Conjunto de Teste: %d" % len(xTeste))
 dfResultado, dfResumo = testar(modelo, xTeste, yTeste)
 
 plotTreinoTeste(dfResultado.index.values, dfResultado["PREVISTO"],
-dfResultado["ESPERADO"], title="CONSUMO DE ENERGIA - RF")
+                dfResultado["ESPERADO"], title="CONSUMO DE ENERGIA - RF")
 print(dfResumo.head())
